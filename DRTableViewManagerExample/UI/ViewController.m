90817766dfbf85972f65b8d4abac08668e12a19c
Dariusz Rybicki <darrarski@gmail.com>
2015-03-02 21:14:09 +0100

    ViewController - Example

--- DRTableViewManagerExample/UI/ViewController.m
+++ DRTableViewManagerExample/UI/ViewController.m
@@ -7,21 +7,152 @@
 //
 
 #import "ViewController.h"
+#import "DRTableViewManager.h"
+#import "DRTableViewGenericSectionsController.h"
+#import "DRTableViewGenericSection.h"
+#import "DRTableViewGenericRow.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) DRTableViewManager *tableViewManager;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"DRTableViewManagerExample";
+    [self.tableViewManager registerInTableView:self.tableView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (DRTableViewManager *)tableViewManager
+{
+    if (_tableViewManager == nil) {
+        DRTableViewGenericSectionsController *sectionsController = [[DRTableViewGenericSectionsController alloc] init];
+        sectionsController.sectionsArray = (NSArray<DRTableViewSection> *)@[
+            [DRTableViewGenericSection createWithBlock:^(DRTableViewGenericSection *section) {
+                section.titleForHeaderBlock = ^NSString * { return @"Section 0 Header"; };
+                section.heightForHeaderBlock = ^CGFloat { return 30; };
+                section.estimatedHeightForHeaderBlock = ^CGFloat { return 30; };
+                section.titleForFooterBlock = ^NSString * { return @"Section 0 Footer"; };
+                section.heightForFooterBlock = ^CGFloat { return 30; };
+                section.estimatedHeightForFooterBlock = ^CGFloat {return 30; };
+                section.rowsArray = (NSArray<DRTableViewRow> *)@[
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S0 R0";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S0 R1";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S0 R2";
+                            return cell;
+                        };
+                    }]
+                ];
+            }],
+            [DRTableViewGenericSection createWithBlock:^(DRTableViewGenericSection *section) {
+                section.titleForHeaderBlock = ^NSString * { return @"Section 1 Header"; };
+                section.heightForHeaderBlock = ^CGFloat { return 30; };
+                section.estimatedHeightForHeaderBlock = ^CGFloat { return 30; };
+                section.titleForFooterBlock = ^NSString * { return @"Section 1 Footer"; };
+                section.heightForFooterBlock = ^CGFloat { return 30; };
+                section.estimatedHeightForFooterBlock = ^CGFloat {return 30; };
+                section.rowsArray = (NSArray<DRTableViewRow> *)@[
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S1 R0";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S1 R1";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S1 R2";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S1 R3";
+                            return cell;
+                        };
+                    }]
+                ];
+            }],
+            [DRTableViewGenericSection createWithBlock:^(DRTableViewGenericSection *section) {
+                section.titleForHeaderBlock = ^NSString * { return @"Section 2 Header"; };
+                section.heightForHeaderBlock = ^CGFloat { return 30; };
+                section.estimatedHeightForHeaderBlock = ^CGFloat { return 30; };
+                section.titleForFooterBlock = ^NSString * { return @"Section 2 Footer"; };
+                section.heightForFooterBlock = ^CGFloat { return 30; };
+                section.estimatedHeightForFooterBlock = ^CGFloat {return 30; };
+                section.rowsArray = (NSArray<DRTableViewRow> *)@[
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S2 R0";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S2 R1";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S2 R2";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S2 R3";
+                            return cell;
+                        };
+                    }],
+                    [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+                        row.cellBlock = ^UITableViewCell * {
+                            UITableViewCell *cell = [[UITableViewCell alloc] init];
+                            cell.textLabel.text = @"S2 R4";
+                            return cell;
+                        };
+                    }]
+                ];
+            }]
+        ];
+
+        _tableViewManager = [[DRTableViewManager alloc] initWithSectionsController:sectionsController];
+    }
+    
+    return _tableViewManager;
 }
 
 @end
