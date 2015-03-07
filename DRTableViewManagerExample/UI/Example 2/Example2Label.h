37fe7e92304d0f7fdf7978ad7f285dcdd7e0f60d
Dariusz Rybicki <darrarski@gmail.com>
2015-03-07 02:21:57 +0100

    Example2Label class - automatically handles preferredMaxLayoutWidth for UILabel under iOS 7

---
+++ DRTableViewManagerExample/UI/Example 2/Example2Label.h
@@ -0,0 +1,13 @@
+//
+//  Example2Label.h
+//  DRTableViewManagerExample
+//
+//  Created by Dariusz Rybicki on 07/03/15.
+//  Copyright (c) 2015 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Example2Label : UILabel
+
+@end
---
+++ DRTableViewManagerExample/UI/Example 2/Example2Label.m
@@ -0,0 +1,35 @@
+//
+//  Example2Label.m
+//  DRTableViewManagerExample
+//
+//  Created by Dariusz Rybicki on 07/03/15.
+//  Copyright (c) 2015 Darrarski. All rights reserved.
+//
+
+#import "Example2Label.h"
+
+@implementation Example2Label
+
+- (void)setBounds:(CGRect)bounds
+{
+    [super setBounds:bounds];
+    [self updatePreferredMaxLayoutWidthIfNeeded];
+}
+
+- (void)setNumberOfLines:(NSInteger)numberOfLines
+{
+    [super setNumberOfLines:numberOfLines];
+    [self updatePreferredMaxLayoutWidthIfNeeded];
+}
+
+- (void)updatePreferredMaxLayoutWidthIfNeeded
+{
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+        if (self.numberOfLines == 0 && (self.bounds.size.width != self.preferredMaxLayoutWidth || self.bounds.size.width == 0)) {
+            self.preferredMaxLayoutWidth = self.bounds.size.width;
+            [self setNeedsUpdateConstraints];
+        }
+    }
+}
+
+@end
