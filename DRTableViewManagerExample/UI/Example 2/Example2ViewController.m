e8083dfcc82e9614d0190e3c261a7e064ab1f1af
Dariusz Rybicki <darrarski@gmail.com>
2015-03-07 01:13:38 +0100

    Example 2 - updated with iOS 7 & 8 compatibility for automatic cell height

--- DRTableViewManagerExample/UI/Example 2/Example2ViewController.m
+++ DRTableViewManagerExample/UI/Example 2/Example2ViewController.m
@@ -33,29 +33,55 @@
 - (DRTableViewManager *)tableViewManager
 {
     if (_tableViewManager == nil) {
+        DRTableViewGenericRow *row = [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
+
+            Example2TableViewCell *(^configureCellForIndexPath)(Example2TableViewCell *, NSIndexPath *);
+            configureCellForIndexPath = ^Example2TableViewCell *(Example2TableViewCell *cell, NSIndexPath *indexPath) {
+                NSMutableArray *textComponents = [NSMutableArray new];
+                [textComponents addObject:[NSString stringWithFormat:@"%ld.", (long)indexPath.row+1]];
+                for (int i=0; i<=indexPath.row; i++) {
+                    [textComponents addObject:@"Lorem ipsum dolor sit amet."];
+                }
+                cell.exampleLabel.text = [textComponents componentsJoinedByString:@" "];
+
+                return cell;
+            };
+
+            row.tableViewHeightForRowAtIndexPathBlock = ^CGFloat(UITableView *tableView, NSIndexPath *indexPath) {
+                if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1) {
+                    Example2TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+                    CGRect frame = cell.frame;
+                    frame.size.width = tableView.bounds.size.width;
+                    cell.frame = frame;
+                    cell = configureCellForIndexPath(cell, indexPath);
+                    [cell setNeedsLayout];
+                    [cell layoutIfNeeded];
+                    CGFloat height = [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height + 1.f;
+
+                    return height;
+                }
+                else {
+                    return UITableViewAutomaticDimension;
+                }
+            };
+
+            row.tableViewCellForRowAtIndexPathBlock = ^UITableViewCell *(UITableView *tableView, NSIndexPath *indexPath) {
+                Example2TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+                cell = configureCellForIndexPath(cell, indexPath);
+
+                return cell;
+            };
+        }];
+
+
         DRTableViewGenericSectionsController *sectionsController = [[DRTableViewGenericSectionsController alloc] init];
         sectionsController.sectionsArray = @[
             [DRTableViewGenericSection createWithBlock:^(DRTableViewGenericSection *section) {
                 section.tableViewNumberOfRowsInSectionBlock = ^NSInteger(UITableView *tableView, NSInteger tableViewSection) {
-                    return 10;
+                    return 20;
                 };
                 section.rowAtIndexBlock = ^NSObject <DRTableViewRow> *(NSInteger rowIndex) {
-                    return [DRTableViewGenericRow createWithBlock:^(DRTableViewGenericRow *row) {
-                        row.tableViewHeightForRowAtIndexPathBlock = ^CGFloat(UITableView *tableView, NSIndexPath *indexPath) {
-                            return UITableViewAutomaticDimension;
-                        };
-                        row.tableViewCellForRowAtIndexPathBlock = ^UITableViewCell *(UITableView *tableView, NSIndexPath *indexPath) {
-                            Example2TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-                            NSMutableArray *textComponents = [NSMutableArray new];
-                            [textComponents addObject:[NSString stringWithFormat:@"%ld.", (long)indexPath.row+1]];
-                            for (int i=0; i<=indexPath.row; i++) {
-                                [textComponents addObject:@"Lorem ipsum dolor sit amet."];
-                            }
-                            cell.exampleLabel.text = [textComponents componentsJoinedByString:@" "];
-                            
-                            return cell;
-                        };
-                    }];
+                    return row;
                 };
             }]
         ];
