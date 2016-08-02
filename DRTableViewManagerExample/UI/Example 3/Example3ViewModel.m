621b64370273acbfa2218a5624287062044d8121
Dariusz Rybicki <darrarski@gmail.com>
2016-08-03 00:48:26 +0200

    Add objects events methods to Example 3 view model

--- DRTableViewManagerExample/UI/Example 3/Example3ViewModel.m
+++ DRTableViewManagerExample/UI/Example 3/Example3ViewModel.m
@@ -54,31 +54,46 @@
 
 - (void)setObjects:(NSArray *)objects
 {
+    [self willChangeObjects];
     _objects = [objects mutableCopy];
+    [self didSetObjects];
+    [self didChangeObjects];
 }
 
 - (void)insertObject:(NSObject *)object atIndex:(NSUInteger)index
 {
+    [self willChangeObjects];
     [_objects insertObject:object atIndex:index];
+    [self didInsertObjectAtIndex:index];
+    [self didChangeObjects];
 }
 
 - (void)removeObjectAtIndex:(NSUInteger)index
 {
+    [self willChangeObjects];
     [_objects removeObjectAtIndex:index];
+    [self didRemoveObjectAtIndex:index];
+    [self didChangeObjects];
 }
 
 - (void)replaceObjectAtIndex:(NSUInteger)index withObject:(id)object
 {
+    [self willChangeObjects];
     id replacedObject = _objects[index];
     _objects[index] = object;
+    [self didReplaceObject:replacedObject atIndex:index];
+    [self didChangeObjects];
 }
 
 - (void)moveObjectAtIndex:(NSUInteger)index1 toIndex:(NSUInteger)index2
 {
     if (index2 == index1) return;
+    [self willChangeObjects];
     NSObject *object = _objects[index1];
     [_objects removeObjectAtIndex:index1];
     [_objects insertObject:object atIndex:index2];
+    [self didMoveObjectAtIndex:index1 toIndex:index2];
+    [self didChangeObjects];
 }
 
 - (void)exchangeObjectAtIndex:(NSUInteger)index1 withObjectAtIndex:(NSUInteger)index2
@@ -87,6 +102,43 @@
     [self moveObjectAtIndex:index1 toIndex:index2];
     [self moveObjectAtIndex:index1 < index2 ? index2 - 1 : index2 + 1
                     toIndex:index1];
+}
+
+#pragma mark - Objects events
+
+- (void)willChangeObjects
+{
+
+}
+
+- (void)didChangeObjects
+{
+
+}
+
+- (void)didSetObjects
+{
+
+}
+
+- (void)didInsertObjectAtIndex:(NSUInteger)index
+{
+
+}
+
+- (void)didRemoveObjectAtIndex:(NSUInteger)index
+{
+
+}
+
+- (void)didReplaceObject:(id)replacedObject atIndex:(NSUInteger)index
+{
+
+}
+
+- (void)didMoveObjectAtIndex:(NSUInteger)index1 toIndex:(NSUInteger)index2
+{
+    
 }
 
 @end
