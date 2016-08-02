b376e19b895ff65b24ff11a506dd1e5a0050775b
Dariusz Rybicki <darrarski@gmail.com>
2016-08-02 20:25:54 +0200

    Add Example 3 view controller class

---
+++ DRTableViewManagerExample/UI/Example 3/Example3ViewController.h
@@ -0,0 +1,10 @@
+//
+// Created by Dariusz Rybicki on 02/08/16.
+// Copyright (c) 2016 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Example3ViewController : UITableViewController
+
+@end
---
+++ DRTableViewManagerExample/UI/Example 3/Example3ViewController.m
@@ -0,0 +1,14 @@
+//
+// Created by Dariusz Rybicki on 02/08/16.
+// Copyright (c) 2016 Darrarski. All rights reserved.
+//
+
+#import "Example3ViewController.h"
+
+@interface Example3ViewController ()
+
+@end
+
+@implementation Example3ViewController
+
+@end
