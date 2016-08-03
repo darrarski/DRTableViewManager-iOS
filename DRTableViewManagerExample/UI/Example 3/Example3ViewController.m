2313d8086f84f9350212c5e12255bfe7328a59c8
Dariusz Rybicki <darrarski@gmail.com>
2016-08-04 01:41:29 +0200

    Remove Example3ViewController conformance to ObservableArrayObserver protocol

--- DRTableViewManagerExample/UI/Example 3/Example3ViewController.m
+++ DRTableViewManagerExample/UI/Example 3/Example3ViewController.m
@@ -11,7 +11,7 @@
 #import "GenericObservableArray.h"
 #import "ObservableArrayObserversSet.h"
 
-@interface Example3ViewController () <ObservableArrayObserver, UIActionSheetDelegate>
+@interface Example3ViewController () <UIActionSheetDelegate>
 
 @property (nonatomic, strong) DRTableViewManager *tableViewManager;
 @property (nonatomic, strong) NSObject <ObservableArray, ObservableMutableArray> *words;
@@ -29,7 +29,6 @@
                                                                                            target:self
                                                                                            action:@selector(openMenu)];
 
-    [self.wordsObservers addObserver:self];
 
     [self.tableViewManager registerInTableView:self.tableView];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
@@ -114,54 +113,6 @@
         _wordsObservers = [ObservableArrayObserversSet new];
     }
     return _wordsObservers;
-}
-
-#pragma mark - ObservableArrayObserver
-
-- (void)willChangeObjects
-{
-    [self.tableView beginUpdates];
-}
-
-- (void)didChangeObjects
-{
-    [self.tableView endUpdates];
-}
-
-- (void)didSetObjects
-{
-    [self.tableView reloadData];
-}
-
-- (void)didInsertObjectAtIndex:(NSUInteger)index
-{
-    [self.tableView insertRowsAtIndexPaths:@[[self tableViewIndexPathFromObjectIndex:index]]
-                          withRowAnimation:UITableViewRowAnimationAutomatic];
-}
-
-- (void)didRemoveObjectAtIndex:(NSUInteger)index
-{
-    [self.tableView deleteRowsAtIndexPaths:@[[self tableViewIndexPathFromObjectIndex:index]]
-                          withRowAnimation:UITableViewRowAnimationAutomatic];;
-}
-
-- (void)didReplaceObject:(id)replacedObject atIndex:(NSUInteger)index
-{
-    [self.tableView reloadRowsAtIndexPaths:@[[self tableViewIndexPathFromObjectIndex:index]]
-                          withRowAnimation:UITableViewRowAnimationAutomatic];
-}
-
-- (void)didMoveObjectAtIndex:(NSUInteger)index1 toIndex:(NSUInteger)index2
-{
-    [self.tableView moveRowAtIndexPath:[self tableViewIndexPathFromObjectIndex:index1]
-                           toIndexPath:[self tableViewIndexPathFromObjectIndex:index2]];
-}
-
-#pragma mark - Helpers
-
-- (NSIndexPath *)tableViewIndexPathFromObjectIndex:(NSUInteger)index
-{
-    return [NSIndexPath indexPathForRow:index inSection:0];
 }
 
 #pragma mark - Actions menu
